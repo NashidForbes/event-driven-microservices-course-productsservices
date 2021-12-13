80c0910a2533ef2986d4e0a87c9882bc839e2906
NashidForbes <NashidForbes@biohumans.com>
2021-12-13 16:55:00 -0500

    Accepting HTTP Request Body in POST createProduct command.  Adding Axon Framework Spring Boot Starter.  Creating a CreateProductRest Modle.  Creating a new CreateProductCommand Object.  Sending Commands to a Command Gateway in ProductsService Controller.

---
+++ src/main/java/com/appsdeveloperblog/estore/productsservice/command/CreateProductCommand.java
@@ -0,0 +1,20 @@
+package com.appsdeveloperblog.estore.productsservice.command;
+
+import lombok.Builder;
+import lombok.Data;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+import java.math.BigDecimal;
+
+// a holder of information to create a new product
+// a command object
+@Builder
+@Data
+public class CreateProductCommand {
+
+    @TargetAggregateIdentifier
+    private final String productId;
+    private final String title;
+    private final BigDecimal price;
+    private final Integer quantity;
+}
--- src/main/java/com/appsdeveloperblog/estore/productsservice/rest/ProductsController.java
+++ src/main/java/com/appsdeveloperblog/estore/productsservice/rest/ProductsController.java
@@ -1,33 +1,56 @@
 package com.appsdeveloperblog.estore.productsservice.rest;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.appsdeveloperblog.estore.productsservice.command.CreateProductCommand;
+import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/products")
 public class ProductsController {
 
-    @Autowired
-    private Environment env;
+    private final Environment env;
+    private final CommandGateway commandGateway;
+
+    //@Autowired not needed since Spring 4.3
+    public ProductsController(Environment env, CommandGateway commandGateway) {
+        this.env = env;
+        this.commandGateway = commandGateway;
+    }
+
 
     @PostMapping
-    public String createProduct(){
-        return "HTTP POST Handled";
+    public String createProduct(@RequestBody CreateProductRestModel createProductRestModel) {
+        CreateProductCommand createProductCommand = CreateProductCommand.builder().price(createProductRestModel.getPrice())
+                .quantity(createProductRestModel.getQuantity())
+                .title(createProductRestModel.getTitle())
+                .productId(UUID.randomUUID().toString()).build();
+
+        String returnValue;
+
+        try {
+            returnValue = commandGateway.sendAndWait(createProductCommand);
+        } catch (Exception ex) {
+            returnValue = ex.getLocalizedMessage();
+        }
+
+        return returnValue;
     }
 
     @GetMapping
-    public String getProduct(){
+    public String getProduct() {
         return "HTTP GET Handled " + env.getProperty("local.server.port");
     }
 
     @PutMapping
-    public String updateProduct(){
+    public String updateProduct() {
         return "HTTP PUT Handled";
     }
 
     @DeleteMapping
-    public String deleteProduct(){
+    public String deleteProduct() {
         return "HTTP DELETE Handled";
     }
 }
