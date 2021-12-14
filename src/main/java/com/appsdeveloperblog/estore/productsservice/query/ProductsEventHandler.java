a0b3e8943badf79e6865a1d1f4db1b512e414448
NashidForbes <NashidForbes@biohumans.com>
2021-12-14 00:52:29 -0500

    Implementing @EventHandler to save ProductEvent data to the database via ProductRepository interface injection.

--- src/main/java/com/appsdeveloperblog/estore/productsservice/query/ProductsEventHandler.java
+++ src/main/java/com/appsdeveloperblog/estore/productsservice/query/ProductsEventHandler.java
@@ -1,7 +1,11 @@
 package com.appsdeveloperblog.estore.productsservice.query;
 
+import com.appsdeveloperblog.estore.productsservice.core.data.ProductEntity;
+import com.appsdeveloperblog.estore.productsservice.core.data.ProductsRepository;
 import com.appsdeveloperblog.estore.productsservice.core.events.ProductCreatedEvent;
 import org.axonframework.eventhandling.EventHandler;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 //Handles ProductEvents and initiates an action
@@ -9,8 +13,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class ProductsEventHandler {
 
+    private final ProductsRepository productsRepository;
+
+    public ProductsEventHandler(ProductsRepository productsRepository) {
+        this.productsRepository = productsRepository;
+    }
+
     @EventHandler
     public void on(ProductCreatedEvent event){
+        ProductEntity productEntity = new ProductEntity();
+        BeanUtils.copyProperties(event, productEntity);
 
+        productsRepository.save(productEntity);
     }
 }
