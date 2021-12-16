634470c5402b8c10fd4dde8d053cb60e3b5f09cc
NashidForbes <NashidForbes@biohumans.com>
2021-12-16 01:32:05 -0500

    Get Products web service endpoint and querying the QueryGateway.

--- src/main/java/com/appsdeveloperblog/estore/productsservice/query/ProductsQueryController.java
+++
@@ -1,9 +0,0 @@
-package com.appsdeveloperblog.estore.productsservice.query;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/products")
-public class ProductsQueryController {
-}
---
+++ src/main/java/com/appsdeveloperblog/estore/productsservice/query/rest/FindProductsQuery.java
@@ -0,0 +1,12 @@
+package com.appsdeveloperblog.estore.productsservice.query.rest;
+
+import lombok.Builder;
+import lombok.Data;
+
+// a holder of information to query a product
+// a query object
+@Builder
+@Data
+public class FindProductsQuery {
+
+}
