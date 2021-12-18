e32bc9a173156169dd81be8f77b8817ad80dd936
NashidForbes <NashidForbes@biohumans.com>
2021-12-18 02:08:11 -0500

    Enabling bean validation in Gradle packages and adding Bean validation for validating Request Body of ProductsCommandController.

--- src/main/java/com/appsdeveloperblog/estore/productsservice/command/rest/models/CreateProductRestModel.java
+++ src/main/java/com/appsdeveloperblog/estore/productsservice/command/rest/models/CreateProductRestModel.java
@@ -2,12 +2,19 @@ package com.appsdeveloperblog.estore.productsservice.command.rest.models;
 
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import java.math.BigDecimal;
 
 @Data
 public class CreateProductRestModel {
 
+    @NotBlank(message="Product title is a required field")
     private String title;
+    @Min(value=1, message="Product price cannot be lower than 1")
     private BigDecimal price;
+    @Min(value=1, message="Product quantity cannot be lower than 1")
+    @Max(value=5, message="Product quantity cannot be greater than 5")
     private Integer quantity;
 }
