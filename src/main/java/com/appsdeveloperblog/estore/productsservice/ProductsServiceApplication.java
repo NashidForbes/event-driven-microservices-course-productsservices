8f2cee3510d2eee4064112413764c8b0606b214b
NashidForbes <NashidForbes@biohumans.com>
2021-12-19 23:45:14 -0500

    Register Message Dispatch Interceptor.

--- src/main/java/com/appsdeveloperblog/estore/productsservice/ProductsServiceApplication.java
+++ src/main/java/com/appsdeveloperblog/estore/productsservice/ProductsServiceApplication.java
@@ -1,8 +1,12 @@
 package com.appsdeveloperblog.estore.productsservice;
 
+import com.appsdeveloperblog.estore.productsservice.command.interceptors.CreateProductCommandInterceptor;
+import org.axonframework.commandhandling.CommandBus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ApplicationContext;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -12,4 +16,9 @@ public class ProductsServiceApplication {
         SpringApplication.run(ProductsServiceApplication.class, args);
     }
 
+    // Register Message Dispatch Interceptor
+    @Autowired
+    public void registerCreateProductCommandInterceptor(ApplicationContext context, CommandBus commandBus) {
+        commandBus.registerDispatchInterceptor(context.getBean(CreateProductCommandInterceptor.class));
+    }
 }
