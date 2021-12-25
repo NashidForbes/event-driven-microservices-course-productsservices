7c5bf3fda361be66fddd00fe7eef8f1218eb0ee9
NashidForbes <NashidForbes@biohumans.com>
2021-12-25 00:07:52 -0500

    Creating exception handlers for different types of exceptions.

--- src/main/java/com/appsdeveloperblog/estore/productsservice/core/handlers/ProductsEventHandler.java
+++ src/main/java/com/appsdeveloperblog/estore/productsservice/core/handlers/ProductsEventHandler.java
@@ -4,6 +4,7 @@ import com.appsdeveloperblog.estore.productsservice.core.data.ProductEntity;
 import com.appsdeveloperblog.estore.productsservice.core.data.interfaces.ProductsRepository;
 import com.appsdeveloperblog.estore.productsservice.core.events.ProductCreatedEvent;
 import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.messaging.interceptors.ExceptionHandler;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
@@ -18,11 +19,28 @@ public class ProductsEventHandler {
         this.productsRepository = productsRepository;
     }
 
+    @ExceptionHandler(resultType=IllegalArgumentException.class)
+    public void handle(IllegalArgumentException exception) {
+        // Log error message
+
+    }
+
+    @ExceptionHandler(resultType=Exception.class)
+    public void handle(Exception exception) {
+        // Log error message
+
+    }
+
     @EventHandler
-    public void on(ProductCreatedEvent event){
+    public void on(ProductCreatedEvent event) {
         ProductEntity productEntity = new ProductEntity();
         BeanUtils.copyProperties(event, productEntity);
 
-        productsRepository.save(productEntity);
+        try {
+            productsRepository.save(productEntity);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
     }
 }
