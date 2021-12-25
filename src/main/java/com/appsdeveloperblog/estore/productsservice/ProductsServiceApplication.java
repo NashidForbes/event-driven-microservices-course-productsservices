8c2e046ea7c9026289067b4f7250af826e5c2703
NashidForbes <NashidForbes@biohumans.com>
2021-12-25 00:32:21 -0500

    Register ListenerInvocationErrorHandler, e.g. first step for creating a error handler to throw exceptions on events and to help start roll back of transactions.

--- src/main/java/com/appsdeveloperblog/estore/productsservice/ProductsServiceApplication.java
+++ src/main/java/com/appsdeveloperblog/estore/productsservice/ProductsServiceApplication.java
@@ -1,7 +1,10 @@
 package com.appsdeveloperblog.estore.productsservice;
 
 import com.appsdeveloperblog.estore.productsservice.command.interceptors.CreateProductCommandInterceptor;
+import com.appsdeveloperblog.estore.productsservice.core.handlers.errors.ProductsServiceEventsErrorHandler;
 import org.axonframework.commandhandling.CommandBus;
+import org.axonframework.config.EventProcessingConfigurer;
+import org.axonframework.eventhandling.PropagatingErrorHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,4 +24,17 @@ public class ProductsServiceApplication {
     public void registerCreateProductCommandInterceptor(ApplicationContext context, CommandBus commandBus) {
         commandBus.registerDispatchInterceptor(context.getBean(CreateProductCommandInterceptor.class));
     }
+
+    // Register ListenerInvocationErrorHandler for rolling back transactions
+    // as part of processing group product-group"
+    @Autowired
+    public void configure(EventProcessingConfigurer config){
+        config.registerListenerInvocationErrorHandler("product-group",
+                conf -> new ProductsServiceEventsErrorHandler());
+
+        //If you do not need to create and register a custom error class then you can provide one provided by Axon framework.
+/*        config.registerListenerInvocationErrorHandler("product-group",
+                conf -> PropagatingErrorHandler.instance());*/
+    }
+
 }
